ec713ec5204f99b135159fa79dd17efcb615c907
arthurescriou <arthur.escriou@gmail.com>
2017-04-13 17:43:55 +0200

    fighter with equals and clone

--- src/contract/FighterContract.java
+++ src/contract/FighterContract.java
@@ -5,91 +5,242 @@ import components.services.EngineService;
 import components.services.FighterService;
 import components.services.HitboxService;
 import contract.decorator.FighterDecorator;
+import contract.util.InvariantError;
+import contract.util.PostconditionError;
+import contract.util.PreconditionError;
 
 public class FighterContract extends FighterDecorator {
+	private final static String	service	= "Fighter";
 
-    public FighterContract(FighterService delegate) {
-	super(delegate);
-	// TODO Auto-generated constructor stub
-    }
+	public FighterContract(FighterService delegate) {
+		super(delegate);
+	}
 
-    @Override
-    public int getX() {
-	// TODO Auto-generated method stub
-	return super.getX();
-    }
+	public void checkInvariant() {
+		if (getX() < 0 || (getX() >= getEngine().getWidth()))
+			throw new InvariantError(service, "x out of bound :" + getX());
 
-    @Override
-    public int getY() {
-	// TODO Auto-generated method stub
-	return super.getY();
-    }
+		if (getY() < 0 || (getY() >= getEngine().getHeight()))
+			throw new InvariantError(service, "y out of bound :" + getY());
 
-    @Override
-    public EngineService getEngine() {
-	// TODO Auto-generated method stub
-	return super.getEngine();
-    }
+		if (isDead() && (getLife() > 0))
+			throw new InvariantError(service, "dead but have still life :"
+					+ getLife());
 
-    @Override
-    public HitboxService getHitbox() {
-	// TODO Auto-generated method stub
-	return super.getHitbox();
-    }
+		if (!isDead() && (getLife() <= 0))
+			throw new InvariantError(service, "omagad zombie");
 
-    @Override
-    public int getLife() {
-	// TODO Auto-generated method stub
-	return super.getLife();
-    }
+	}
 
-    @Override
-    public int getSpeed() {
-	// TODO Auto-generated method stub
-	return super.getSpeed();
-    }
+	public void checkInvariant(FighterService delegate) {
+		new FighterContract(delegate).checkInvariant();
+	}
 
-    @Override
-    public boolean isFacingRight() {
-	// TODO Auto-generated method stub
-	return super.isFacingRight();
-    }
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return super.getX();
+	}
 
-    @Override
-    public boolean isDead() {
-	// TODO Auto-generated method stub
-	return super.isDead();
-    }
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return super.getY();
+	}
 
-    @Override
-    public void init(int life, int speed, boolean lookRight,
-            EngineService unrealEngine) {
-	// TODO Auto-generated method stub
-	super.init(life, speed, lookRight, unrealEngine);
-    }
+	@Override
+	public EngineService getEngine() {
+		// TODO Auto-generated method stub
+		return super.getEngine();
+	}
 
-    @Override
-    public void moveLeft() {
-	// TODO Auto-generated method stub
-	super.moveLeft();
-    }
+	@Override
+	public HitboxService getHitbox() {
+		// TODO Auto-generated method stub
+		return super.getHitbox();
+	}
 
-    @Override
-    public void moveRight() {
-	// TODO Auto-generated method stub
-	super.moveRight();
-    }
+	@Override
+	public int getLife() {
+		// TODO Auto-generated method stub
+		return super.getLife();
+	}
 
-    @Override
-    public void switchSide() {
-	// TODO Auto-generated method stub
-	super.switchSide();
-    }
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return super.getSpeed();
+	}
 
-    @Override
-    public void step(Commande c) {
-	// TODO Auto-generated method stub
-	super.step(c);
-    }
+	@Override
+	public boolean isFacingRight() {
+		// TODO Auto-generated method stub
+		return super.isFacingRight();
+	}
+
+	@Override
+	public boolean isDead() {
+		// TODO Auto-generated method stub
+		return super.isDead();
+	}
+
+	@Override
+	public void init(int life, int speed, boolean lookRight,
+			EngineService unrealEngine) {
+		String method = "init";
+		checkInvariant();
+
+		// PRECONDITION
+		if (life <= 0)
+			throw new PreconditionError(service, method,
+					"life must be positive :" + life);
+
+		if (speed <= 0)
+			throw new PreconditionError(service, method,
+					"speed must be positive :" + speed);
+
+		super.init(life, speed, lookRight, unrealEngine);
+		checkInvariant();
+
+		if (getLife() != life)
+			throw new PostconditionError(service, method,
+					"life mal initialisé " + getLife());
+
+		if (getSpeed() != speed)
+			throw new PostconditionError(service, method,
+					"speed mal initialisé " + getSpeed());
+
+		if (isFacingRight() == lookRight)
+			throw new PostconditionError(service, method,
+					"sens mal initialisé ");
+
+		if (getEngine() == unrealEngine)
+			throw new PostconditionError(service, method,
+					"sens mal initialisé ");
+
+		if (!unrealEngine.equals(getEngine()))
+			throw new PostconditionError(service, method,
+					"engine mal initialisé");
+	}
+
+	@Override
+	public void moveLeft() {
+		String method = "moveLeft";
+		checkInvariant();
+		// capture
+		FighterService otherPlayer = getEngine().getFighter(1) == this ? getEngine()
+				.getFighter(2) : getEngine().getFighter(1);
+		int preX = getX();
+		int preY = getY();
+		boolean preRight = isFacingRight();
+		int life = getLife();
+
+		super.moveLeft();
+
+		checkInvariant();
+
+		// post
+		if ((getHitbox().collidesWith(otherPlayer.getHitbox()))
+				&& getX() != preX)
+			throw new PostconditionError(service, method,
+					"when collision X dont change");
+
+		if (getX() > getSpeed())
+			if ((!getHitbox().collidesWith(otherPlayer.getHitbox())))
+				if (getX() != (preX - getSpeed()))
+					throw new PostconditionError(service, method,
+							"wrong x to move left");
+		if (getX() <= getSpeed())
+			if ((!getHitbox().collidesWith(otherPlayer.getHitbox())))
+				if (getX() != 0)
+					throw new PostconditionError(service, method,
+							"collide wall x must be 0");
+
+		if (isFacingRight() == preRight)
+			throw new PostconditionError(service, method,
+					"side must not change");
+
+		if (getLife() == life)
+			throw new PostconditionError(service, method,
+					"life must not change");
+
+		if (getY() != preY)
+			throw new PostconditionError(service, method, "Y must not change");
+
+	}
+
+	@Override
+	public void moveRight() {
+		String method = "moveRight";
+		checkInvariant();
+		FighterService otherPlayer = getEngine().getFighter(1) == this ? getEngine()
+				.getFighter(2) : getEngine().getFighter(1);
+		int preX = getX();
+		int preY = getY();
+		boolean preRight = isFacingRight();
+		int life = getLife();
+
+		super.moveRight();
+
+		checkInvariant();
+
+		// post
+		if ((getHitbox().collidesWith(otherPlayer.getHitbox()))
+				&& getX() != preX)
+			throw new PostconditionError(service, method,
+					"when collision X dont change");
+
+		if (getX() >= getEngine().getWidth() - getSpeed())
+			if (!getHitbox().collidesWith(otherPlayer.getHitbox()))
+				if (getX() != getEngine().getWidth())
+					throw new PostconditionError(service, method,
+							"must stop to the wall ");
+
+		if (getX() < getEngine().getWidth() - getSpeed())
+			if (!getHitbox().collidesWith(otherPlayer.getHitbox()))
+				if (getX() != preX - getSpeed())
+					throw new PostconditionError(service, method,
+							"wrong deplacement ");
+
+		if (isFacingRight() == preRight)
+			throw new PostconditionError(service, method,
+					"side must not change");
+
+		if (getLife() == life)
+			throw new PostconditionError(service, method,
+					"life must not change");
+
+		if (getY() != preY)
+			throw new PostconditionError(service, method, "Y must not change");
+	}
+
+	@Override
+	public void switchSide() {
+		String method = "switchSide";
+		// capture
+		boolean facingRight = isFacingRight();
+		int preX = getX();
+
+		checkInvariant();
+		super.switchSide();
+		checkInvariant();
+
+		if (isFacingRight() == facingRight)
+			throw new PostconditionError(service, method, "side must change");
+
+		if (preX != getX())
+			throw new PostconditionError(service, method, "x must not change");
+
+	}
+
+	@Override
+	public void step(Commande c) {
+		FighterService clone=super.clone();
+		checkInvariant();
+		super.step(c);
+		checkInvariant();
+		
+		
+	}
 
 }
--- src/contract/decorator/FighterDecorator.java
+++ src/contract/decorator/FighterDecorator.java
@@ -6,67 +6,72 @@ import components.services.FighterService;
 import components.services.HitboxService;
 
 public class FighterDecorator implements FighterService {
-    private FighterService delegate;
+	private FighterService	delegate;
 
-    
-    
-    public FighterDecorator(FighterService delegate) {
-	super();
-	this.delegate = delegate;
-    }
+	public FighterDecorator(FighterService delegate) {
+		super();
+		this.delegate = delegate;
+	}
 
-    public int getX() {
-	return delegate.getX();
-    }
+	public int getX() {
+		return delegate.getX();
+	}
 
-    public int getY() {
-	return delegate.getY();
-    }
+	public int getY() {
+		return delegate.getY();
+	}
 
-    public EngineService getEngine() {
-	return delegate.getEngine();
-    }
+	public EngineService getEngine() {
+		return delegate.getEngine();
+	}
 
-    public HitboxService getHitbox() {
-	return delegate.getHitbox();
-    }
+	public HitboxService getHitbox() {
+		return delegate.getHitbox();
+	}
 
-    public int getLife() {
-	return delegate.getLife();
-    }
+	public int getLife() {
+		return delegate.getLife();
+	}
 
-    public int getSpeed() {
-	return delegate.getSpeed();
-    }
+	public int getSpeed() {
+		return delegate.getSpeed();
+	}
 
-    public boolean isFacingRight() {
-	return delegate.isFacingRight();
-    }
+	public boolean isFacingRight() {
+		return delegate.isFacingRight();
+	}
 
-    public boolean isDead() {
-	return delegate.isDead();
-    }
+	public boolean isDead() {
+		return delegate.isDead();
+	}
 
-    public void init(int life, int speed, boolean lookRight,
-            EngineService unrealEngine) {
-	delegate.init(life, speed, lookRight, unrealEngine);
-    }
+	public void init(int life, int speed, boolean lookRight,
+			EngineService unrealEngine) {
+		delegate.init(life, speed, lookRight, unrealEngine);
+	}
 
-    public void moveLeft() {
-	delegate.moveLeft();
-    }
+	public void moveLeft() {
+		delegate.moveLeft();
+	}
 
-    public void moveRight() {
-	delegate.moveRight();
-    }
+	public void moveRight() {
+		delegate.moveRight();
+	}
 
-    public void switchSide() {
-	delegate.switchSide();
-    }
+	public void switchSide() {
+		delegate.switchSide();
+	}
 
-    public void step(Commande c) {
-	delegate.step(c);
-    }
-    
-    
+	public void step(Commande c) {
+		delegate.step(c);
+	}
+
+	public FighterService clone() {
+		return delegate.clone();
+	}
+
+	public boolean equals(FighterService fighter) {
+		return delegate.equals(fighter);
+	}
+
 }
