d5239f9b175e15ab2cfccf4be372c4c351dfca0f
FouquerYohann <yohann.fouquer@gmail.com>
2017-04-13 15:07:56 +0200

    coucouc

--- src/components/services/FighterService.java
+++ src/components/services/FighterService.java
@@ -61,16 +61,56 @@ public interface FighterService {
      * 		getSpeed()= speed  && <p>
      * 		isFacingRight() == lookRight && <p>
      * 		getEngine()== unrealEngine && <p>
-     * 		∃ hitbox H such as getHitbox()==H
+     * 		∃ hitbox H, such as getHitbox()==H
      * 
      */
     public void init(int life,int speed,boolean lookRight,EngineService unrealEngine);
     
     
     //Operators
+    /**
+     * @post
+     * 		∃ i such as getEngine().getFighter(i) != this && <p>
+     * 		getHitbox().collidesWith(getEngine().getFighter(i).getHitbox())  => getX() == @pre getX() <p>
+     * 		getX() > getSpeed() &&
+     * 		∀ i getEngine().getFighter(i) != this => !getHitbox().collidesWith(getEngine().getFighter(i).getHitox()) => getX()== @pre getX() - getSpeed() 
+     * 		getX() <= getSpeed() && 
+     * 		∀ i getEngine().getFighter(i) != this => !getHitbox().collidesWith(getEngine().getFighter(i).getHitox()) => getX()== 0
+     * 		isFacingRight() == @pre isFacingRight() && getLife() == @pre getLife()
+     * 		getY()= @pre getY()
+     */
     public void moveLeft();
+    /**
+     * @post
+     * 		∃ i such as getEngine().getFighter(i) != this && <p>
+     * 		getHitbox().collidesWith(getEngine().getFighter(i).getHitbox())  => getX() == @pre getX() <p>
+     * 		getX() >= getEngine().getWidth() - getSpeed() &&
+     * 		∀ i getEngine().getFighter(i) != this => !getHitbox().collidesWith(getEngine().getFighter(i).getHitox()) => getX()== getEngine().getWidth() 
+     * 		getX() < getEngine().getWidth() - getSpeed() && 
+     * 		∀ i getEngine().getFighter(i) != this => !getHitbox().collidesWith(getEngine().getFighter(i).getHitox()) => getX()== @pre getX() - getSpeed()
+     * 		isFacingRight() == @pre isFacingRight() && getLife() == @pre getLife()
+     * 		getY()= @pre getY()
+     */
+    
     public void moveRight();
+    
+    /**
+     * 	@post
+     * 		isFacingRight() != @pre isFacingRight()
+     * 		getX() = @pre getX() && getY() = @pre getY()
+     */
     public void switchSide();
+    
+    /**
+     * 
+     * @param c COMMANDE
+     *      
+     * @post 
+     * 		step(LEFT)  == moveLeft()
+     * 		step(RIGHT) == moveRight()
+     * 		step(NEUTRAL) == this
+     */
+    
     public void step(COMMANDE c);
     
 }
