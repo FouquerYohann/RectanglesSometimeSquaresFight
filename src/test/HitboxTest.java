bb5891a92f3caee36071a309c046f11fdb0a11ec
FouquerYohann <yohann.fouquer@gmail.com>
2017-04-21 15:01:08 +0200

    ajout du tiimer

--- src/test/HitboxTest.java
+++ src/test/HitboxTest.java
@@ -1,13 +1,13 @@
 package test;
 
-import org.junit.Before;
+
 
 import components.services.HitboxService;
 
 public class HitboxTest {
 	private HitboxService hitbox;
 	
-	@Before
+	
 	public void beforeTest(){
 		
 	}
